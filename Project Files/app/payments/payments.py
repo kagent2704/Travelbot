875f948f8b0bc32fca801e59280a1a9db4d76e11
Kashmira Patil <kashmira.patil@mitwpu.edu.in>
2025-06-11 22:30:18 +0530

    Update payments.py

--- Project Files/app/payments/payments.py
+++ Project Files/app/payments/payments.py
@@ -1,7 +1,7 @@
 import stripe
 
 # Set your secret key directly (not recommended for production)
-stripe.api_key = "sk_test_51R9KNP3zcDeHgkU0ozizyyUkG2xudrSvGOWIFdwNhH7jyIrFRql0Mm8jKleVwkel5HAtM00hT8TSnFcfU5eh22XQ00IFtp7w83"
+stripe.api_key = "YOUR_STRIPE_API_KEY"
 
 def create_payment_intent(amount, currency="inr"):
     try:
